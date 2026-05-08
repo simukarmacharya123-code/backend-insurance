35707b2aac8ce66a70e13416c6948359969066d2
Simran Karmacharya <simukarmacharya123@gmail.git push -u origin main>
2026-05-08 13:14:38 -0400

    Added TestControllerTest for Junit Testing

--- src/test/java/TestControllerTest.java
+++ src/test/java/TestControllerTest.java
@@ -1,2 +1,20 @@
-public class TestControllerTest {
+package com.simran.insurancebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class TestControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void testHelloApi() {
+        String response = restTemplate.getForObject("/hello", String.class);
+        assertThat(response).isEqualTo("Hello Simran from cloned project!");
+    }
 }
